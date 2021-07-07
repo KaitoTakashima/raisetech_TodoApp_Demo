c51b38de94d41aedec8e0c901c3d34119e916970
KaitoTakashima <61674582+KaitoTakashima@users.noreply.github.com>
2021-07-07 20:28:56 +0900

    complete_btn

--- src/index.js
+++ src/index.js
@@ -20,7 +20,47 @@ const onClickAdd = () => {
   const complete_btn = document.createElement("button");
   complete_btn.className = "button";
   complete_btn.addEventListener("click", () => {
-    alert("完了");
+    deleteFromIncompleteList(delete_btn.closest(".incomplete_item"));
+
+    //完了リストに追加する要素
+    const addTarget = complete_btn.closest(".incomplete_item");
+    //TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerHTML;
+    //li以下を初期化
+    addTarget.textContent = null;
+    addTarget.className = "complete_item list_item";
+
+    //pタグを生成
+    const p = document.createElement("p");
+    p.className = "complete_item_name list_item_name";
+    p.innerText = text;
+    console.log(p);
+
+    //divタグを生成
+    const div = document.createElement("div");
+    div.className = "complete_btn_group";
+
+    //button（戻す）タグを生成
+    const return_btn = document.createElement("button");
+    return_btn.className = "button";
+
+    //imgタグの生成
+    const return_img = document.createElement("img");
+    return_img.src = "src/images/return_img.png";
+
+    //button（戻す）タグの子要素にimgタグ
+    return_btn.appendChild(return_img);
+    console.log(return_btn);
+
+    //divタグの子要素にbuttonタグを追加
+    div.appendChild(return_btn);
+
+    //liタグ以下に各要素を追加
+    addTarget.appendChild(p);
+    addTarget.appendChild(div);
+
+    //完了リストに追加
+    document.getElementById("complete_list").appendChild(addTarget);
   });
 
   //imgタグ（完了）生成
@@ -34,8 +74,7 @@ const onClickAdd = () => {
   delete_btn.className = "button";
   delete_btn.addEventListener("click", () => {
     //押された削除ボタンの親タグ(li)を未完了リストから削除
-    const deleteTarget = delete_btn.closest(".incomplete_item");
-    document.getElementById("incomplete_list").removeChild(deleteTarget);
+    deleteFromIncompleteList(delete_btn.closest(".incomplete_item"));
   });
 
   //imgタグ（削除）生成
@@ -54,6 +93,11 @@ const onClickAdd = () => {
 
   //未完了リストに追加
   document.getElementById("incomplete_list").appendChild(li);
+
+  //未完了リストから指定の要素を削除
+  const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete_list").removeChild(target);
+  };
 };
 
 document
